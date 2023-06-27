b7c0eb0dec6ac3d9ea613eab526598df91688293
Gutis007 <101084525+Gutis-007@users.noreply.github.com>
2023-06-27 10:40:23 -0300

    Add files via upload

--- Modulo 7/Questao 2.py
+++ Modulo 7/Questao 2.py
@@ -1,8 +1,10 @@
-#Questão 1
 largura = int(input("Digite a largura do retângulo: "))
 altura = int(input("Digite a altura do retângulo: "))
 
 for i in range(altura):
     for j in range(largura):
-        print("#", end="")
+        if i == 0 or i == altura - 1 or j == 0 or j == largura - 1:
+            print("#", end="")
+        else:
+            print(" ", end="")
     print()
